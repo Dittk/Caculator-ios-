719a3a320257da48d9365717eefae4fab5f7c595
caa789 <caa789@naver.com>
2022-08-02 10:08:24 +0900

    feat: Add various function
    Start with initialize "0" in result label.
    Reset button color before clicking operator button.
    After operator button click, change properties and result label initialize.
    Saving numeric data when clicking the operator button.

--- Basic_1_Lecture/ViewController.swift
+++ Basic_1_Lecture/ViewController.swift
@@ -11,7 +11,6 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var result: UILabel!
     
-    
     @IBOutlet weak var buttonAc: UIButton!
     @IBOutlet weak var buttonPm: UIButton!
     @IBOutlet weak var buttonPerc: UIButton!
@@ -38,27 +37,29 @@ class ViewController: UIViewController {
     @IBOutlet weak var button11: UIButton!
     @IBOutlet weak var buttonEql: UIButton!
     
-    var result1: String?
-    var result2: String?
-    var sum: Int?
+    var leftTemp: Int?
+    var rightTemp: Int?
+    var resultTemp: Int?
+    
+    var oprCnt: Int = 0
+    var saveCnt: Int = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
     
         result.layer.cornerRadius = 50
-        result.layer.masksToBounds = true // 코너 radius 변경이 안될 시 선언
+        result.layer.masksToBounds = true
+        // 코너 radius 변경이 안될 시 선언
         
     }
 
-    override func viewDidAppear(_ animated: Bool) { // 버튼에 대한 속성으로 접근 시 (button1.bounds.width) appear 후에 적용
+    override func viewDidAppear(_ animated: Bool) { // 버튼에 대한 속성(button1.bounds.width)으로 접근 시  appear 후에 적용
         super.viewDidAppear(animated)
-        
         
         buttonAc.layer.cornerRadius = button1.bounds.width / 2
         buttonPm.layer.cornerRadius = button1.bounds.width / 2
         buttonPerc.layer.cornerRadius = button1.bounds.width / 2
         buttonDiv.layer.cornerRadius = button1.bounds.width / 2
-        
         
         button1.layer.cornerRadius = button1.bounds.width / 2
         button2.layer.cornerRadius = button1.bounds.width / 2
@@ -80,48 +81,214 @@ class ViewController: UIViewController {
         buttonEql.layer.cornerRadius = button1.bounds.width / 2
         
     }
+    
+    // 숫자 버튼 영역 시작
     @IBAction func btn1Action(_ sender: Any) {
+        
+        confirmZero()
+        isEnableOpr()
+        initializeColor()
         result.text?.append("7")
-
+        
     }
+    
     @IBAction func btn2Action(_ sender: Any) {
+        
+        confirmZero()
+        isEnableOpr()
+        initializeColor()
         result.text?.append("8")
+        
     }
     
     @IBAction func btn3Action(_ sender: Any) {
+        
+        confirmZero()
+        isEnableOpr()
+        initializeColor()
         result.text?.append("9")
+        
     }
+    
     @IBAction func btn4Action(_ sender: Any) {
+        
+        confirmZero()
+        isEnableOpr()
+        initializeColor()
         result.text?.append("4")
+        
     }
     
     @IBAction func btn5Action(_ sender: Any) {
+        
+        confirmZero()
+        isEnableOpr()
+        initializeColor()
         result.text?.append("5")
+        
     }
+    
     @IBAction func btn6Action(_ sender: Any) {
+        
+        confirmZero()
+        isEnableOpr()
+        initializeColor()
         result.text?.append("6")
+        
     }
     
     @IBAction func btn7Action(_ sender: Any) {
+        
+        confirmZero()
+        isEnableOpr()
+        initializeColor()
         result.text?.append("1")
+        
     }
+    
     @IBAction func btn8Action(_ sender: Any) {
+        
+        confirmZero()
+        isEnableOpr()
+        initializeColor()
         result.text?.append("2")
+        
     }
+    
     @IBAction func btn9Action(_ sender: Any) {
+        
+        confirmZero()
+        isEnableOpr()
+        initializeColor()
         result.text?.append("3")
+        
     }
+    
     @IBAction func btn0Action(_ sender: Any) {
+        
+        confirmZero()
+        isEnableOpr()
+        initializeColor()
         result.text?.append("0")
+        
     }
     
-    @IBAction func btnDotAction(_ sender: Any) {
-        result.text?.append(".")
+    @IBAction func btnDotAction(_ sender: Any) { //Detail
+       
+        if !(result.text!.contains(".")) {
+            result.text?.append(".")
+        }
+
+    }
+    // 숫자 버튼 영역 끝
+
+    
+    // 연산자 버튼 영역 시작
+    @IBAction func btnACAction(_ sender: Any) {
+        
+        initializeColor()
+        result.text = "0"
+    }
+    
+    @IBAction func btnDivisionAction(_ sender: Any) {
+        
+        oprCnt = 1
+        
+        initializeColor()
+        setColor(button: buttonDiv)
+        
+        
+        
+    }
+    
+    @IBAction func btnMultipleAction(_ sender: Any) {
+        
+        oprCnt = 1
+        
+        initializeColor()
+        setColor(button: buttonMul)
+        
+    }
+    
+    @IBAction func btnMinusAction(_ sender: Any) {
+        
+        oprCnt = 1
+        initializeColor()
+        setColor(button: buttonMin)
+       
 
     }
     
+    @IBAction func btnPlusAction(_ sender: Any) {
+        
+        oprCnt = 1
+        initializeColor()
+        setColor(button: buttonPlu)
+        
+    }
+    
+    @IBAction func btnResultAction(_ sender: Any) {
+        
+    }
+    
+    
+    // 연산자 버튼 영역 끝
     
     
     
+    func initializeColor() { // 연산자 색상 속성 초기화
+        
+        buttonDiv.setTitleColor(.white, for: .normal)
+        buttonDiv.backgroundColor = .systemOrange
+    
+        buttonMul.setTitleColor(.white, for: .normal)
+        buttonMul.backgroundColor = .systemOrange
+        
+        buttonMin.setTitleColor(.white, for: .normal)
+        buttonMin.backgroundColor = .systemOrange
+        
+        buttonPlu.setTitleColor(.white, for: .normal)
+        buttonPlu.backgroundColor = .systemOrange
+        
+    }
+    
+    func setColor(button: UIButton)  { // 연산자 클릭 시 버튼 색 변경
+    
+        button.setTitleColor(.systemOrange, for: .normal)
+        button.backgroundColor = .white
+      
+    }
+    
+    func isEnableOpr(){ // 연산자 클릭 시 label 초기화
+        
+        if oprCnt == 1 {
+            result.text = ""
+            oprCnt = 0
+        }
+    }
+    
+    func confirmZero(){ // 초기 숫자 설정
+        
+        if result.text == "0"{
+            result.text = ""
+        }
+    }
+    
+    func saveTemp(){ // 연산할 데이터 저장
+        
+        if saveCnt == 0{
+            leftTemp = Int(result.text!)
+            saveCnt = 1
+        }
+        
+        else{
+            rightTemp = Int(result.text!)
+            saveCnt = 0
+
+        }
+
+        
+    }
+    
+
 }
-
